66f894b26db4464706407453c5817a0e5a74bbdc
Bhadresh Dudhat <33336465+bhadreshdudhat@users.noreply.github.com>
2017-12-27 22:29:39 -0600

    Add push functionality to Javascript

--- Stack.js
+++ Stack.js
@@ -14,3 +14,23 @@ view=function () {
     var stackpara=document.getElementById('stack');
     stackpara.innerHTML=s1.getDataString();
 }
+
+//Push functionality
+s1.push=function(v){
+    if(v!=""){
+        if(this.sp==0){
+            document.getElementById("stackMessage").innerHTML="Stack is full! No more push Possible";
+        }else{
+            this.sp=this.sp-1;
+            this.stk[this.sp]=v;
+            document.getElementById("stackMessage").innerHTML="You Pushed "+v;
+        }
+    }
+};
+function pushHandler() {
+    var in1=document.getElementById("stackinput");
+    s1.push(in1.value);
+    view();
+    document.getElementById("stackinput").value="";
+    // document.getElementById("stackMessage").innerHTML="You Pushed "+in1.value;
+}
